b666dc40152b4545098a98fec9f3a48e0c599545
K4zBeK <k4zbek@gmail.com>
2021-07-05 03:29:16 +0300

    Create future.py
    look inside!

---
+++ future.py
@@ -0,0 +1,5 @@
+i hope you will die soon you fucking asshole
+burn in hell bitch and your shitty kids will be killed by cancer
+cock sucker
+die asshole!
+Damn you and your damn family! Burn in hell!
